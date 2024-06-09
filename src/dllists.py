264060ebca744a549410e251992d05946fcacee0
qqadi <qaisalqadi2000@gmail.com>
2024-06-09 13:15:51 +0300

    added display_list method, and its unit test

--- src/dllists.py
+++ src/dllists.py
@@ -4,15 +4,12 @@ class Node:
         self.nxt = nxt
         self.prev = prev
 
-    
-
 class DoubleLinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
         self.count = 0
 
-    # step1:
     def append_first(self, data):
         new_node = Node(data)
         if self.head == None:
@@ -24,7 +21,7 @@ class DoubleLinkedList:
             new_node.nxt = self.head
             self.head = new_node
             self.count +=1
-    
+
     def insert_last(self, data):
         new_node = Node(data)
         if self.head == None:
@@ -34,7 +31,8 @@ class DoubleLinkedList:
             new_node.prev = self.tail
             self.tail = new_node
 
-    # def display_list(self):
-    #     curr = self.head
-    #     while curr.nxt:
-    #         curr = curr.nxt
+    def display_list(self):
+        curr = self.head
+        while curr:
+            print(f"<-{curr.data}->", end='' )
+            curr = curr.nxt
--- src/dllists_tests.py
+++ src/dllists_tests.py
@@ -1,7 +1,8 @@
 from dllists import DoubleLinkedList
 
-def test_append_first():
-    dll = DoubleLinkedList()
+dll = DoubleLinkedList()
+
+def test_append_first(dll):
     assert dll.head == None, "header exists in empty list"
     dll.append_first(1)
     assert dll.head.data == 1, "header value doesnt match first append"
@@ -12,5 +13,10 @@ def test_append_first():
     assert dll.tail.data == 1, "order isnt correct"
     assert dll.tail.prev.data == 2, "order isnt correct"
 
+def test_display_list(dll):
+    dll.display_list()
+
+
 if __name__ == "__main__":
-    test_append_first()
+    test_append_first(dll)
+    test_display_list(dll)
